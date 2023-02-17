75ad412c25ba3608463337c16156b127d3735d1f
Kailinho <kailashbala22@gmail.com>
2023-02-17 18:15:13 -0500

    Fixed some spacing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,10 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {backgroundImage : {'logo':"url('./assets/logo.png')"}},
+    extend: {backgroundImage : {'logo':"url('./assets/logo.png')",'hero':"url('./assets/hero.jpg')"
+    
+  
+  }},
     fontFamily:{'serif':['Montserrat']}
   },
   plugins: [require("daisyui")],
